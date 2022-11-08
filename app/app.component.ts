a71c7822c6675271eb573dcea46ba817af720845
YANH <76054703+whaitking@users.noreply.github.com>
2022-11-08 17:04:55 +0100

    08/11 17.04

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { flatMap } from 'rxjs';
 
 interface Mons {
-  name_m: string;
+  month: number;
   holidays_national: number[];
   holidays_regional: number[];
   holidays_local: number[];
   holidays_central: number[];
-  month_days: [];
-  month_days_count: number;
 }
 
 
@@ -21,27 +19,29 @@ export class AppComponent {
   week: string[];
   arrayMons: Mons[];
   months: number[] = [];
-  year:number = 2022;
-  years: Date = new Date(this.year);
+  year: number = 2022;
+  years: Date = new Date(this.year,);
 
   /* words; */
 
   constructor() {
     let arrayJSON: string;
 
-    this.months = [8,9]
+    this.months = [8, 9]
 
 
     arrayJSON = `[
-      {"name_m": "Septiembre", "holidays_regional": [11], "holidays_local": [29], "holidays_central": [0], "holidays_national":[0], "month_days_count": 30},
-      {"name_m": "Octubre", "holidays_regional": [0], "holidays_local": [0], "holidays_central": [31], "holidays_national":[12],"month_days_count": 31}
+      {"month":8, "holidays_regional": [11], "holidays_local": [29], "holidays_central": [0], "holidays_national":[0]},
+      {"month":9, "holidays_regional": [0], "holidays_local": [0], "holidays_central": [31], "holidays_national":[12]}
     ]`;
 
 
     this.arrayMons = JSON.parse(arrayJSON);
     this.week = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
-
+    console.log(this.arrayMons);
   }
+
+
 
   getDaysInYearMonth(year: number, month: number) {
     let date: Date = new Date(year, month);
@@ -49,10 +49,23 @@ export class AppComponent {
     let month_days_count: number = new Date(year, month, 0).getDate();
 
     for (let i = 1; i <= month_days_count; i++) {
-      arrayDays[i-1]=new Date(year,month,i);     
+      arrayDays[i - 1] = new Date(year, month, i);
     }
 
     return arrayDays;
+  }
+
+  getMonthInYear(year: number, month: number) {
+    let date: Date = new Date(year, month);
+    let month_days_count: number = new Date(year, month, 0).getDate();
+
+    return date;
+  }
+
+  checkHoliDay(arrayMons: Mons[], day: number, month: Date) {
+
+
+
   }
 
 }
